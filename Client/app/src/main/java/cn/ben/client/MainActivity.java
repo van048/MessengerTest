7afe4b109fcf47bfcca4762e24b3d574b92b6bb9
dqshare <739362790@qq.com>
2017-03-13 15:51:54 +0800

    init client

---
+++ Client/app/src/main/java/cn/ben/client/MainActivity.java
@@ -0,0 +1,42 @@
+package cn.ben.client;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private Intent getExplicitIntent(@NonNull Context context, Intent implicitIntent) {
+        PackageManager packageManager = context.getPackageManager();
+        if (packageManager == null) return null;
+
+        List<ResolveInfo> resolveInfoList = packageManager.queryIntentServices(implicitIntent, 0);
+        // only have one service matches
+        if (resolveInfoList == null || resolveInfoList.size() != 1) {
+            return null;
+        }
+        ResolveInfo resolveInfo = resolveInfoList.get(0);
+        if (resolveInfo == null || resolveInfo.serviceInfo == null) return null;
+
+        String packageName = resolveInfo.serviceInfo.packageName;
+        String className = resolveInfo.serviceInfo.name;
+        ComponentName componentName = new ComponentName(packageName, className);
+
+        Intent explicitIntent = new Intent(implicitIntent);
+        explicitIntent.setComponent(componentName);
+        return explicitIntent;
+    }
+}
